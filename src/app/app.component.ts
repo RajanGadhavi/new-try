b52f38788d83cae373816b24e76aa9bcaa1b6336
RajanGadhavi <rajangadhavi5@gmail.com>
2025-03-20 21:05:03 +0530

    Add style in change-log.md

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './Components/navbar/navbar.component'
-import { HomepageComponent } from "./Components/homepage/homepage.component";
 import { FooterComponent } from "./Components/footer/footer.component";
-import { ShopComponent } from "./Components/shop/shop.component";
-import { ProductPageComponent } from "./Components/product-page/product-page.component";
-import { CheckoutComponent } from "./Components/checkout/checkout.component";
 
 @Component({
   selector: 'app-root',
